cbec35b69b85977a98f9de6ef1e6396f7a6574e3
Xariskaramf <>
2025-04-14 00:12:56 +0300

    Basic fileds and methods for parent class

--- Entities/src/Parent.java
+++ Entities/src/Parent.java
@@ -1,4 +1,36 @@
+import java.util.Date;
+
+//Super class that includes fields that will appear on most entities.
 
 public class Parent {
 
+	private String id;
+	private Date dateCreated;
+	private Date dateUpdated;
+	
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public Date getDateUpdated() {
+		return dateUpdated;
+	}
+
+	public void setDateUpdated(Date dateUpdated) {
+		this.dateUpdated = dateUpdated;
+	}
+	
 }
